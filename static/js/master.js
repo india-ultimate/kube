11833b557cb5d69848ca5d0234c9057ffff2a5e3
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-02-25 17:19:29 +0530

    Try to use a pre-computed index, if available

--- static/js/master.js
+++ static/js/master.js
@@ -1,53 +1,75 @@
-$.get(document.currentScript.getAttribute("data-index"))
-  .done(function(documents) {
-    var index = lunr(function() {
-      this.ref("uri");
-      this.field("title", { boost: 10 });
-      this.field("content");
-      documents.map(function(doc) {
-        this.add(doc);
-      }, this);
-    });
-    $("#search").keyup(function(e) {
-      if (e.which !== 13) {
-        return;
-      }
-      var results = index.search(this.value),
-        docs = results.slice(0, 5).map(function(result) {
-          return documents.filter(function(doc) {
-            return doc.uri == result.ref;
-          })[0];
-        });
+var index;
 
-      var modal_body = $(".modal-body");
-      modal_body.children().remove();
-      docs.forEach(function(doc) {
-        $("<p>")
-          .append(
-            $("<a>")
-              .attr("href", doc.uri)
-              .text(doc.title)
-          )
-          .append($("<span style='display: block;'>").text(doc.author))
-          .append(
-            $("<span style='display: block;'>").text("Issue " + doc.issue)
-          )
-          .appendTo(modal_body);
+var setup_search = function(documents) {
+  $("#search").keyup(function(e) {
+    if (e.which !== 13) {
+      return;
+    }
+    var results = index.search(this.value),
+      docs = results.slice(0, 5).map(function(result) {
+        return documents.filter(function(doc) {
+          return doc.uri == result.ref;
+        })[0];
       });
 
-      $("#search-modal .close").click(function() {
+    var modal_body = $(".modal-body");
+    modal_body.children().remove();
+    docs.forEach(function(doc) {
+      $("<p>")
+        .append(
+          $("<a>")
+            .attr("href", doc.uri)
+            .text(doc.title)
+        )
+        .append($("<span style='display: block;'>").text(doc.author))
+        .append($("<span style='display: block;'>").text("Issue " + doc.issue))
+        .appendTo(modal_body);
+    });
+
+    $("#search-modal .close").click(function() {
+      $("#search-modal").addClass("hide");
+    });
+
+    $("#search-modal").removeClass("hide");
+    $(document).keydown(function(e) {
+      if (e.which === 27) {
         $("#search-modal").addClass("hide");
-      });
-
-      $("#search-modal").removeClass("hide");
-      $(document).keydown(function(e) {
-        if (e.which === 27) {
-          $("#search-modal").addClass("hide");
-        }
-      });
+      }
     });
-  })
-  .error(function(e) {
-    console.log("Couldn't load index.json");
-    console.log(e);
+  });
+};
+
+var lumr_index = document.currentScript.getAttribute("data-lumr-index"),
+  raw_index = document.currentScript.getAttribute("data-index");
+
+var setup_raw_index = function(documents) {
+  console.log("Setting up raw index...");
+  index = lunr(function() {
+    this.ref("uri");
+    this.field("title", { boost: 10 });
+    this.field("content");
+    documents.map(function(doc) {
+      this.add(doc);
+    }, this);
+    setup_search(documents);
+  });
+};
+
+var setup_lumr_index = function(data) {
+  console.log("Setting up lumr index...");
+  index = lunr.Index.load(data);
+  $.get(raw_index).success(function(documents) {
+    setup_search(documents);
+  });
+};
+
+$.get(lumr_index)
+  .success(setup_lumr_index)
+  .fail(function(e) {
+    $.get(raw_index)
+      .success(setup_raw_index)
+      .fail(function(e) {
+        console.log("Couldn't load index.json");
+        console.log(e);
+      });
   });
